ae588e5139f933e409060cc91be424bb036dcf1b
nigustsegay <82320345+nigustsegay@users.noreply.github.com>
2022-01-13 09:08:38 +0300

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php get_header();?>
+<section class = 'page-wrap'>
+<dev class = 'container'>
+
+<h1> <?phpthe_tytle();?></h1>
+
+<?php get_template_part('inc/section', 'content');?>
+
+
+</dev>
+</section>
+
+
+<?php get_footer();?>
