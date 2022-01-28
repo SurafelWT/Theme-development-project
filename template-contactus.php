d8e3a68e37c444e74ce600d2eef6cd330d3df849
wesentalem <59748935+wesentalem@users.noreply.github.com>
2022-01-28 17:03:03 +0300

    Add files via upload

---
+++ template-contactus.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Contact Us
+*/
+?>
+
+<?php get_header();?>
+
+<section class = "page-wrap">
+
+<div class = "container">
+
+    <h1><?php the_title();?></h1>
+    <div class = "row">
+
+             <div class="col-lg-6">
+                 this is where the contact us gose
+             </div>
+
+             <div class="col-lg-6">
+             <?php get_template_part('inc/section', 'content');?>
+
+             </div>
+
+    </div>
+
+</div>
+</section>
+
+<?php get_footer();?>
